eb656a03527f686558f800644f2be9b3d61bad05
Thomas <collinskthomas@gmail.com>
2023-07-11 11:08:48 +0000

    Add support to initialise object from dict(kwargs)

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+This module defines the BaseModel class
+"""
 import uuid
 from datetime import datetime
 
@@ -13,16 +16,28 @@ class BaseModel:
         updated_at (datetime): Date & time when the instance was last updated.
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializes a new instance of BaseModel.
 
         The id is set to a unique identifier generated using UUID.
         The created_at & updated_at attributes are set to current date & time.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    # skip this attribute
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    # convert value from string to datetime object
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+
+                # set attribute of object using key & value
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
@@ -30,7 +45,7 @@ class BaseModel:
 
         Returns:
             str: String representation of the instance in the format:
-                "[ClassName] (id) additional_info"
+                "[ClassName] (id) dictionary_representation"
         """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
@@ -38,7 +53,7 @@ class BaseModel:
         """
         Updates the updated_at attribute with the current date and time.
 
-        This method is called to mark the instance as updated.
+        This method is called to mark when an instance is updated.
         """
         self.updated_at = datetime.now()
 
@@ -53,8 +68,16 @@ class BaseModel:
         Returns:
             dict: Dictionary representation of the instance.
         """
+
+        # create a shallow copy of __dict__ for modification
         new_dict = self.__dict__.copy()
+
+        # add a __class__ field
         new_dict["__class__"] = self.__class__.__name__
+
+        # modify the created_at & updated_fields from datetime
+        # objects into strings in ISO format
         new_dict["created_at"] = self.created_at.isoformat()
         new_dict["updated_at"] = self.updated_at.isoformat()
+
         return new_dict
