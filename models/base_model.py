1cc9457ebd45961145735b6c8408cdcda2f5b816
Thomas <collinskthomas@gmail.com>
2023-07-12 20:02:38 +0000

    Add unit tests for BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -38,7 +38,7 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
 
             # save the new instance to storage
             models.storage.new(self)
