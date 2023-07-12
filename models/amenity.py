14a10b24d018c8132f1ce7e4e04ac94240dc6bbe
Adooz <kndonake@gmail.com>
2023-07-12 02:50:53 +0000

    Task 7 though 10 Created commands for console.py, wrote new classes that inherits from BaseModel and updated filestorage/console.py to accept the new classes

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/pyhton3
+"""This module defines a class `Amenity`"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    '''Defines a Amenity'''
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/pyhton3
+"""This module defines a class `State`"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    '''Defines a State'''
+    name = ""
